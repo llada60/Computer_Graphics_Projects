24a5f2141c43aab3ea253109b0569946599f482d
llada60 <lingada60@gmail.com>
2025-06-18 22:33:53 +0200

    Update

--- Shadow/src/fragmentShaderShadowMap.glsl
+++
@@ -1,9 +0,0 @@
-#version 330 core            // minimal GL version support expected from the GPU
-
-// output data
-layout(location = 0) out float fragmentDepth;
-
-void main(){
-  // not really needed, OpenGL does it anyway
-  fragmentDepth = gl_FragCoord.z;
-}
